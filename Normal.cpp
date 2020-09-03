2aa427b3a926b0dbeeda70f601627f014fa456b8
Kirspig <kirspig@gmail.com>
2020-09-03 22:53:52 +0300

    ОАиП лаба 1

---
+++ Normal.cpp
@@ -0,0 +1,23 @@
+﻿#define _USE_MATH_DEFINES 
+#include<iostream> 
+#include<stdio.h> 
+#include<math.h> 
+
+
+using namespace std;
+
+int main()
+{
+    double x, y, b, z1, z2;
+    cout << "Введите произвольное число: ";
+    while (!(cin >> b)) {
+        cout << "Вы ввели не число";
+        return 0;
+    }
+    x = (pow(cos((3 / 8.) * M_PI - (b / 4.)), 2));
+    y = (pow(cos((11 / 8.) * M_PI + (b / 4.)), 2));
+    z1 = (x - y);
+    z2 = (sqrt(2) / 2) * sin(b / 2.0);
+    cout << z1 << " = " << z2;
+    return 0;
+}
