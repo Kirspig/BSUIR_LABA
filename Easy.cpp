d8771dd84bfcaff3849611171f66f17933955feb
Kirspig <70720137+Kirspig@users.noreply.github.com>
2020-09-05 20:13:57 +0300

    ОАиП лаба 1

--- Easy.cpp
+++ Easy.cpp
@@ -1,4 +1,4 @@
-﻿#define _USE_MATH_DEFINES
+#define _USE_MATH_DEFINES
 #include<iostream>
 #include<stdio.h>
 #include<math.h>
@@ -7,16 +7,22 @@ using namespace std;
 
 int main()
 {
-    double x, y, a, z1, z2;
+    setlocale(LC_ALL, "Russian");
+    double x, y, a, z1, z2, pi;
     cout << "Введите произвольное число: ";
     while (!(cin >> a)) {
         cout << "Вы ввели не число";
         return 0;
     }
-    x = (sin(4 * a)) / (1 + cos(4 * a));
-    y = (cos(2 * a)) / (1 + cos(2 * a));
-    z1 = x * y;
-    z2 = cos(1.5 * M_PI - a) / sin(1.5 * M_PI - a);
-    cout << z1 << " = " << z2;
+    if (cos(4 * a) == -1 or cos(2 * a) == -1 or sin(1.5 * M_PI - a) == 0) {
+        cout << "Нарушены математические правила";
+    }
+    else {
+        x = (sin(4 * a)) / (1 + cos(4 * a));
+        y = (cos(2 * a)) / (1 + cos(2 * a));
+        z1 = x * y;
+        z2 = cos(1.5 * M_PI - a) / sin(1.5 * M_PI - a);
+        cout << z1 << " = " << z2;
+    }
     return 0;
-} 
+}
