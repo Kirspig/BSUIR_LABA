536fd6ef07ed18abfccb84ecc7126589ec97c640
Kirspig <70720137+Kirspig@users.noreply.github.com>
2020-09-06 15:20:25 +0300

    ОАиП лаба 2

--- Easy_2.cpp
+++ Easy_2.cpp
@@ -16,20 +16,12 @@ int main() {
         return 0;
     }
     if (input > 0) {
-        key = 1;
+        cout << "z >0 \n";
+            x = 1 / (input * input + 2 * input) ;
     }
     else {
-        key = 2;
-    }
-    switch (key) {
-        case 1:
-            cout << "z >0 \n";
-            x = 1 / (input * input + 2 * input) ;
-            break;
-        case 2:
-            cout << "z <=0 \n";
+        cout << "z <=0 \n";
             x = 1 - input * input * input;
-            break;
     }
     if ((input * input + 2 * input) == 0 or (log(abs(x)) + x) == 0) {
         cout << "Нарушены математические правила";
